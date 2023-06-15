28da1dc9d1359b61f61210dec2ab223926527bac
BrianPark314 <everfrost314@gmail.com>
2023-06-15 09:23:49 +0900

    django day 3

--- Django/Django/board/views.py
+++ Django/Django/board/views.py
@@ -1,6 +1,49 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Question, Answer
+from django.utils import timezone
+from .forms import QuestionForm, AnswerForm
+from django.http import HttpResponseNotAllowed
+from django.core.paginator import Paginator
 # Create your views here.
 
 def index(request):
-    return HttpResponse("안녕하세요. board의 index 페이지입니다.")
+    page = request.GET.get('page', '1')
+    #return HttpResponse("안녕하세요. board의 index 페이지입니다.")
+    question_list = Question.objects.order_by('-created_at')
+    paginator = Paginator(question_list, 10)
+    page_obj = paginator.get_page(page)
+    context = {'question_list': page_obj}
+    return render(request, 'board/question_list.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'board/question_detail.html', context)
+
+def answer_create(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    if request.method == 'POST':
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.created_at = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect('board:detail', question_id=question.id)
+    else:
+        return HttpResponseNotAllowed('Only POST is available.')
+    context = {'question':question, 'form':form}
+    return render(request, 'board/question_detail.html', context)
+    
+def question_create(request):
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.created_at = timezone.now()
+            question.save()
+            return redirect('board:index')
+    else:
+        form = QuestionForm()
+    context = {'form':form}
+    return render(request, 'board/question_form.html', context)
--- Django/Django/config/settings.py
+++ Django/Django/config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "common.apps.CommonConfig",
     "board.apps.BoardConfig",
     "django.contrib.admin",
     "django.contrib.auth",
@@ -55,7 +56,7 @@ ROOT_URLCONF = "config.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / 'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -117,6 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
